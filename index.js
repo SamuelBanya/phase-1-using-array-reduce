29510fc44a315247f9080dcaca43fef2491c089d
Samuel Banya <sbanya@fastmail.com>
2022-05-03 21:05:21 -0400

    commit tree 1f7ae7b498be8a0afc2e3b77ea82a4562b70c63f

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+// Normal version
+// const doubledAndSummed = [1, 2, 3].reduce(function(accumulator, element){ return element * 2 + accumulator}, 0)
+
+const totalBatteries = batteryBatches.reduce(function(accumulator, element){ return accumulator + element}, 0);
+
+console.log(`totalBatteries: ${totalBatteries}`);
+
+// Crazy version aka ignore:
+// incomingStudents.reduce(function(showcases, student) { showcases[studentSorter.showcaseAssign(student)].push(student)}, allShowcases)
